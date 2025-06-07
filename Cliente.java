301b159aa60158a61a864adfa0b82932edb3c353
Andrea <amourinodesousa@danielcastelao.com>
2025-06-07 17:56:59 +0200

    programar cliente

--- Cliente.java
+++ Cliente.java
@@ -1,2 +1,35 @@
+
 public class Cliente {
+    private String nombre;
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - " + email;
+    }
 }
+
